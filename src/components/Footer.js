8a58ae6349e056f1dec7e19da80a23bcd646c905
sauravraj3dev <saurav.raj@newfold.com>
2021-10-13 18:01:14 +0530

    Second Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -64,11 +64,11 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
+              SAURAV
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>SAURAV © 2021</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
